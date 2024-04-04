8984447b8e4d445b9228e02987848d454a846134
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-04 10:42:16 +0700

    Membuat prosedur display

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];//Array yang akan dicari
-int n;		//Nomor elemen pada array
-int i;		//Indeks elemen pada array
+int arr[20]; //Array yang akan dicari
+int n; //Nomor elemen pada array
+int i; //Indeks elemen pada array
+
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+}
