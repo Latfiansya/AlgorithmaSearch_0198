08472e9990574901ab9388b281c050cc9c3f6e6e
Latfiansya <latfiansyadzikri@gmail.com>
2024-04-04 10:53:30 +0700

    Memasukkan input elemen pada array

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -16,4 +16,15 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	//Menerima elemen pada array
+	cout << "\n-----------------------";
+	cout << "\n Enter array elements \n";
+	cout << "-----------------------\n\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
 }
+
